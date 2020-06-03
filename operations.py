f215a32ba252ccdf0527b7fbc50f39a55bd3375c
cherviralasaipranathi <saipranathichervirala@gmail.com>
2020-06-03 16:27:51 +0530

    Fourth commit

---
+++ operations.py
@@ -0,0 +1,12 @@
+def add(x,y):
+    return x+y
+def wish(name):
+    print("Good Afternoon",name)
+def even(num):
+    if(num%2==0):
+        print("even")
+    else:
+        print("odd")
+def details(dept,college):
+              print("Department and college",dept,college)
+result = "True"
